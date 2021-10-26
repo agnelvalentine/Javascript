6e446ee3bbf9a037573808fc5a76d4771e0b7e34
Agnel Valentine Douglas A <avdagnel.a@gmail.com>
2021-10-26 19:29:37 +0530

    Hi

--- JS Adv Functions/caching.js
+++ JS Adv Functions/caching.js
@@ -64,33 +64,33 @@
 
 // console.log(worker.slow(2)); // works
 // console.log(worker.slow(2));
-let worker = {
-    slow(min, max) {
-        console.log(`Called with ${min},${max}`);
-        return min + max;
-    }
-};
+// let worker = {
+//     slow(min, max) {
+//         console.log(`Called with ${min},${max}`);
+//         return min + max;
+//     }
+// };
 
-function cachingDecorator(func, hash) {
-    let cache = new Map();
-    return function() {
-        let key = hash(arguments); // (*)
-        if (cache.has(key)) {
-            return cache.get(key);
-        }
+// function cachingDecorator(func, hash) {
+//     let cache = new Map();
+//     return function() {
+//         let key = hash(arguments); // (*)
+//         if (cache.has(key)) {
+//             return cache.get(key);
+//         }
 
-        let result = func.call(this, ...arguments); // (**)
+//         let result = func.call(this, ...arguments); // (**)
 
-        cache.set(key, result);
-        return result;
-    };
-}
+//         cache.set(key, result);
+//         return result;
+//     };
+// }
 
-function hash(args) {
-    return args[0] + ',' + args[1];
-}
+// function hash(args) {
+//     return args[0] + ',' + args[1];
+// }
 
-worker.slow = cachingDecorator(worker.slow, hash);
+// worker.slow = cachingDecorator(worker.slow, hash);
 
-console.log(worker.slow(3, 5)); // works
-console.log("Again " + worker.slow(3, 5)); // same (cached)
+// console.log(worker.slow(3, 5)); // works
+// console.log("Again " + worker.slow(3, 5)); // same (cached)
